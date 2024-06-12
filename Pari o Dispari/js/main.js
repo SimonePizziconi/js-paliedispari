ca65d0796a73252438ab2e691de4c898645a654c
SimonePizziconi <simonepizziconi05@gmail.com>
2024-06-12 17:43:59 +0200

    feat: create function for show random pcNumber

--- Pari o Dispari/js/main.js
+++ Pari o Dispari/js/main.js
@@ -2,5 +2,14 @@
 let userChoice = prompt("pari o dispari?");
 
 //  e di scegliere un numero da 1 a 5
-let userNumber = prompt("scegli un numero da 1 a 5");
+let userNumber = parseInt(prompt("scegli un numero da 1 a 5"));
 console.log(userChoice, userNumber);
+
+// crea funzione per generare un numero casuale da 1 a 5
+function randomNumber (min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// genera un numero casuale per il pc
+let pcNumber = randomNumber(1, 5);
+console.log(pcNumber);
