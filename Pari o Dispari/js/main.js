f75ff15ed33bab4df24124cf17e86880122102b7
SimonePizziconi <simonepizziconi05@gmail.com>
2024-06-12 17:39:20 +0200

    feat: ask the user to choose odd or even and a number

--- Pari o Dispari/js/main.js
+++ Pari o Dispari/js/main.js
@@ -1,4 +1,6 @@
 // chiedi all'utente di scegliere pari o dispari
-let userChoice = prompt("pari o dispari?")
+let userChoice = prompt("pari o dispari?");
+
 //  e di scegliere un numero da 1 a 5
-let 
+let userNumber = prompt("scegli un numero da 1 a 5");
+console.log(userChoice, userNumber);
