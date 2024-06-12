64a824cfcf437a8ef9e05665595451272c812982
SimonePizziconi <simonepizziconi05@gmail.com>
2024-06-12 17:30:13 +0200

    feat: create function and use function for show message

--- Palindroma/js/main.js
+++ Palindroma/js/main.js
@@ -2,6 +2,25 @@
 let userWord = prompt("Inserisci una parola");
 console.log(userWord);
 
+// crea una funzione per verificare che la parola si palindroma
+function palindromeWord(word) {
+    // Confronta la parola pulita con la sua versione invertita
+    const parolaInversa = word.split('').reverse().join('');
+    
+    return word === parolaInversa;
+}
+
+// usiamo la funzione
+if (palindromeWord(userWord)) {
+    // Stampa un messaggio appropiato
+    console.log(`La parola '${userWord}' è un palindromo.`);
+} else {
+    // Stampa un messaggio appropiato
+    console.log(`La parola '${userWord}' non è un palindromo.`);
+}
+
+
+
 
 
 
